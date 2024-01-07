bd4b29baca310fe056fa0e74b3a10f8aa17e2808
amadejzr <amadej@jerlah.si>
2024-01-07 21:16:21 +0100

    Updated flutter_new_release it checks if branch exists locally

--- flutter_new_release.sh
+++ flutter_new_release.sh
@@ -20,8 +20,8 @@ then
     error_exit "git could not be found. Please install git."
 fi
 
-# Fetch the latest main branch
-git checkout main && git pull origin main
+# Fetch the latest main branch and all remote branches
+git fetch --all
 
 # Check for release name argument
 if [ $# -eq 0 ]
@@ -30,21 +30,32 @@ if [ $# -eq 0 ]
 fi
 
 RELEASE_NAME=$1
+RELEASE_BRANCH="release/$RELEASE_NAME"
+
+# Check if the release branch already exists locally
+if git branch --list $RELEASE_BRANCH | grep -q $RELEASE_BRANCH; then
+    error_exit "The release branch '$RELEASE_BRANCH' already exists locally. Choose a different name or delete the existing branch if it's no longer needed."
+fi
+
+# Check if the release branch already exists on remote
+if git ls-remote --heads origin $RELEASE_BRANCH | grep -q $RELEASE_BRANCH; then
+    error_exit "The release branch '$RELEASE_BRANCH' already exists on remote. Choose a different name or delete the existing branch if it's no longer needed."
+fi
 
 # Create a new release branch from the main
-git checkout -b release/$RELEASE_NAME main
+git checkout -b $RELEASE_BRANCH main
 
 if [ $? -ne 0 ]; then
     error_exit "Failed to create a new release branch. Make sure the branch name doesn't conflict with existing branches."
 fi
 
-print_message 2 "A new release branch 'release/$RELEASE_NAME' has been created successfully from the main branch."
+print_message 2 "A new release branch '$RELEASE_BRANCH' has been created successfully from the main branch."
 
 # Push the new release branch to remote
-git push -u origin release/$RELEASE_NAME
+git push -u origin $RELEASE_BRANCH
 
 if [ $? -ne 0 ]; then
     error_exit "Failed to push the new release branch to the remote repository."
 fi
 
-print_message 2 "The release branch 'release/$RELEASE_NAME' has been pushed to the remote repository successfully."
+print_message 2 "The release branch '$RELEASE_BRANCH' has been pushed to the remote repository successfully."
