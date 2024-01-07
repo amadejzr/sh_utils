80029631cbd4671f264426f0e001647c8775aa55
amadejzr <amadej@jerlah.si>
2024-01-07 19:45:40 +0100

    Updating package script

--- flutter_create_package.sh
+++ flutter_create_package.sh
@@ -31,6 +31,8 @@ flutter create --template=package "$name" || handle_error
 # Navigate into the package directory.
 cd "$name" || handle_error
 
+echo "$(tput setaf 3)Creating dir screenshots and files README and CHANGELOG$(tput sgr0)"
+
 # Create README.md with the specified content.
 echo "TODO: UPDATE READ ME" > README.md || handle_error
 
@@ -40,11 +42,14 @@ echo "TODO: UPDATE CHANGE LOG" > CHANGELOG.md || handle_error
 # Create the 'screenshots' directory.
 mkdir screenshots || handle_error
 
+
+echo "$(tput setaf 3)Creating a new Flutter project inside the 'example' directory...$(tput sgr0)"
+
 # Create the 'example' directory and navigate into it.
 mkdir example && cd example || handle_error
 
 # Create a new Flutter project inside the 'example' directory.
-flutter create . || handle_error
+flutter create . > /dev/null 2>&1 || handle_error
 
 # Navigate back to the main package directory.
 cd .. || handle_error
@@ -61,8 +66,6 @@ sed -i '' "/dependencies:/a\\
     path: ../
 " example/pubspec.yaml || handle_error
 
-# Change the description in the example's pubspec.yaml.
-sed -i '' "s/description: A new Flutter project./description: Example for $name/" example/pubspec.yaml || handle_error
-
 # Print completion message.
-echo "Flutter package '$name' has been created successfully with all specified files and configurations."
+echo "$(tput setaf 2)Flutter package '$name' has been created successfully with all specified files and configurations.$(tput sgr0)"
+
